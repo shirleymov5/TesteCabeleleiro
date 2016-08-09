a28727f328c95b2014569163ee2b62ce37c28cc9
shirleymov5 <shirley@mov5.com.br>
2016-08-09 04:30:59 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/mov5/testecabeleleiro/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.mov5.testecabeleleiro;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/mov5/testecabeleleiro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mov5.testecabeleleiro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
